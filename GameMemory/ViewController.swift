25ac2f5ea1eb6aa9119afc3831dd79e03dd7a35c
aluno <aluno@PRAIADOCAVADO.local>
2018-04-14 13:38:27 -0300

    Initial Commit

---
+++ GameMemory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameMemory
+//
+//  Created by aluno on 14/04/18.
+//  Copyright © 2018 aluno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
